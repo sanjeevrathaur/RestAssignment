846df8cfdc32a3c2bf04d02246869e67886ab37a
Sanjeev Rathaur <sanjeevrrathaur@gmail.com>
2017-09-29 02:23:47 +0530

    commit stable code

--- src/main/java/com/test/data/source/FileDataSource.java
+++ src/main/java/com/test/data/source/FileDataSource.java
@@ -3,6 +3,7 @@ package com.test.data.source;
 import com.test.utils.Constant;
 import jdk.nashorn.internal.parser.JSONParser;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
@@ -51,9 +52,9 @@ public class FileDataSource {
         return file;
     }
 
-    public synchronized File readObject(Object object) throws Exception{
+    public synchronized /*File*/ JSONArray readObjects() throws Exception{
 
-        File file = openFile();
+/*        File file = openFile();
         FileInputStream inStream = new FileInputStream(file);
         byte dataBuffer[] = new byte[inStream.available()];
         inStream.read(dataBuffer);
@@ -66,13 +67,55 @@ public class FileDataSource {
             String name = json.get("customerName").toString();
             System.out.println("name : "+ name);
             String amount = json.get("amount").toString();
-        }
+        }*/
 
         // convert dataJSONAsStr into JSONArray
         // iterate over array to get a record in basis of getID
         //return the Object
 
-        return file;
+
+        org.json.JSONArray employeeList = null;
+        org.json.simple.parser.JSONParser jsonParser = new org.json.simple.parser.JSONParser();
+        try (FileReader reader = new FileReader(Constant.DATA_SOURCE_FILE_LOCATION))
+        {
+            //Read JSON file
+            if(reader.read() == -1){
+                return employeeList;
+            }
+
+            File files =  new File("D:\\inteilljwork\\database.json");
+
+            FileInputStream is = new FileInputStream(files);
+            is = new FileInputStream(files);
+            byte dataBuffer[] = new byte[is.available()];
+            is.read(dataBuffer);
+            is.close();
+            String dataJSONAsStr = new String(dataBuffer);
+            System.out.println("json data str : "+ dataJSONAsStr);
+
+
+            employeeList = new org.json.JSONArray(dataJSONAsStr);
+            System.out.println("jArray : "+employeeList);
+
+            System.out.println(employeeList);
+
+            System.out.println("Size of list is : "+employeeList.length());
+
+            //Iterate over employee array
+            //employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } /*catch (ParseException e) {
+            e.printStackTrace();
+        } */catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return employeeList;
+
+        /*return file;*/
     }
 
 
@@ -100,6 +143,51 @@ public class FileDataSource {
         //return the Object
 
         return count;
+    }
+
+    public String checkRecord(int id){
+
+        String result = "No record found for this id.";
+
+        try{
+            org.json.JSONArray allCustomerAccounts = readObjects();
+            for (int i=0; i<=allCustomerAccounts.length(); i++){
+                JSONObject jsonObject = (JSONObject) allCustomerAccounts.get(i);
+                JSONObject jsonObjectNew = (JSONObject)jsonObject.get("account");
+                int objectId = jsonObjectNew.getInt("id");
+                if(objectId == id){
+                    return jsonObject.toString();
+                }
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String updateRecordPartialy(Json){
+
+
+    }
+
+    private void parseEmployeeObject(org.json.simple.JSONObject employee)
+    {
+        //Get employee object within list
+        org.json.simple.JSONObject employeeObject = (org.json.simple.JSONObject) employee.get("account");
+
+        //Get employee first name
+        String firstName = (String) employeeObject.get("customerName");
+        System.out.println(firstName);
+
+        //Get employee last name
+        String lastName = (String) employeeObject.get("currency");
+        System.out.println(lastName);
+
+        //Get employee website name
+        Double website = (Double) employeeObject.get("amount");
+        System.out.println(website);
     }
 
 }
--- src/main/java/com/test/rest/service/controller/AccountRestController.java
+++ src/main/java/com/test/rest/service/controller/AccountRestController.java
@@ -3,6 +3,8 @@ package com.test.rest.service.controller;
 import com.test.data.source.FileDataSource;
 import com.test.model.Account;
 import com.test.utils.Constant;
+import com.test.utils.PATCH;
+import org.json.JSONException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,6 +13,7 @@ import org.json.simple.parser.ParseException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.*;
 
 // Plain old Java Object it does not extend as class or implements
@@ -23,46 +26,64 @@ import java.io.*;
 // The browser requests per default the HTML MIME type.
 
 //Sets the path to base URL + /hello
-@Path("/hello")
+@Path("/account")
 public class AccountRestController {
 
-    // This method is called if TEXT_PLAIN is request
+    //  TODO : Need to inject dependency via Spring
+    FileDataSource fileDataSource = new FileDataSource();
+
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String sayPlainTextHello() {
-        return "Hello Jersey";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllCustommerAccounts() {
+
+        String message = "No Record Found !";
+        try{
+            org.json.JSONArray allCustomerAccounts = fileDataSource.readObjects();
+            return Response.status(200).entity(allCustomerAccounts).build();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return Response.status(200).entity(message).build();
+    }
+
+    @GET
+    @Path("{accountId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustommerAccount(@PathParam("accountId")int accountId) {
+
+        return Response.status(200).entity(fileDataSource.checkRecord(accountId)).build();
     }
 
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String saveRecord(String accountData) {
+    public Response saveRecord(String accountData) {
 
         System.out.println(accountData);
         com.fasterxml.jackson.databind.ObjectMapper objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
 
-
-        FileWriter fileWriter = null;
-
         try{
             Account account = objectMapper.readValue(accountData, Account.class);
             JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", account.getId());
             jsonObject.put("customerName", account.getCustomerName());
             jsonObject.put("currency", account.getCurrency());
             jsonObject.put("amount", account.getAmount());
             JSONObject tempObject = new JSONObject();
             tempObject.put("account", jsonObject);
 
-            JSONArray jsonArray = readJsonFile();
+            org.json.JSONArray jsonArray = fileDataSource.readObjects();
             if(jsonArray == null){
-                jsonArray = new JSONArray();
+                jsonArray = new org.json.JSONArray();
             }
-            jsonArray.add(tempObject);
+            jsonArray.put(tempObject);
 
-            try (FileWriter file = new FileWriter("D:\\inteilljwork\\database.json")) {
-
-                file.write(jsonArray.toJSONString());
+            try (FileWriter file = new FileWriter(Constant.DATA_SOURCE_FILE_LOCATION)) {
+                file.write(jsonArray.toString());
                 file.flush();
 
             } catch (IOException e) {
@@ -72,60 +93,18 @@ public class AccountRestController {
 
         }catch (IOException e){
             e.printStackTrace();
-        }
-
-
-        return "Hello Jersey";
-    }
-
-    private JSONArray readJsonFile(){
-
-        JSONArray employeeList = null;
-        JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader("D:\\inteilljwork\\database.json"))
-        {
-            //Read JSON file
-            if(reader.read() == -1){
-                return employeeList;
-            }
-            employeeList = (JSONArray)jsonParser.parse(reader);
-
-            new JSONArray("xcvcx");
-            //employeeList = (JSONArray) obj;
-            System.out.println(employeeList);
-
-            System.out.println("Size of list is : "+employeeList.size());
-
-            //Iterate over employee array
-            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+        }catch (Exception e) {
             e.printStackTrace();
         }
-        return employeeList;
+        return Response.status(200).entity("Record Save Successfuly").build();
     }
 
-    private void parseEmployeeObject(JSONObject employee)
-    {
-        //Get employee object within list
-        JSONObject employeeObject = (JSONObject) employee.get("account");
+    @PATCH
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response partialUpdateCustommerAccount(String dataForUpdate) {
 
-        //Get employee first name
-        String firstName = (String) employeeObject.get("customerName");
-        System.out.println(firstName);
 
-        //Get employee last name
-        String lastName = (String) employeeObject.get("currency");
-        System.out.println(lastName);
-
-        //Get employee website name
-        Double website = (Double) employeeObject.get("amount");
-        System.out.println(website);
+        return Response.status(200).entity("Record updated successfully").build();
     }
-
 
 }
